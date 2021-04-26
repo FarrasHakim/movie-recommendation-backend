017d40c12dd71decaaa56f00c96fa3a08ff4bfd0
Farras Hakim <farrashakim@gmail.com>
2021-04-26 16:06:43 +0700

    Base api website

---
+++ README.md
@@ -0,0 +1,11 @@
+# Movie Recommendation API Service
+Proyek ini dibuat menggunakan SWI-Prolog dalam rangka memenuhi tugas akhir mata kuliah Pemrograman Logika. Proyek ini berperan sebagai service yang menyediakan data movie yang akan digunakan oleh sebuah frontend framework.
+
+#### How to run
+1. Clone proyek
+2. Pastikan swi-prolog sudah terinstall
+3. Buka cmd/terminal di folder 'movie-recommendaiton-backend'
+4. Jalankan `swipl web-service.pl`
+5. SWI-Prolog akan berjalan di cmd/terminal
+6. Jalankan syntax `server(<port>)`
+7. Website dapat diakses melalui browser dengan alamat url `localhost:<port>`
---
+++ web-service.pl
@@ -0,0 +1,65 @@
+:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_error)).
+:- use_module(library(http/http_json)).
+:- [database].
+
+http:location(files, '/f', []).
+http:location(food, '/food', []).
+
+% Declare a handler, binding an HTTP path to a predicate.
+% The notation root(hello_world) uses an alias-mechanism similar to
+% absolute_file_name/3 and allows for moving parts of the server locations
+% easily. See http_absolute_location/3.
+:- http_handler(root(.), say_hi, []).
+
+% And, just for clarity, define a second handler
+% this one can by reached at http://127.0.0.1:8000/taco
+:- http_handler(food(tacos), say_taco, []).
+:- http_handler(food(kebabs), say_kebab, []).
+:- http_handler('/add', handle_request, []).
+
+% The predicate server(?Port) starts the server. It simply creates a
+% number of Prolog threads and then returns to the toplevel, so you can
+% (re-)load code, debug, etc.
+server(Port) :-
+        http_server(http_dispatch, [port(Port)]).
+
+/* The implementation of /hello_world. The single argument provides the request
+details, which we ignore for now. Our task is to write a CGI-Document:
+a number of name: value -pair lines, followed by two newlines, followed
+by the document content, The only obligatory header line is the
+Content-type: <mime-type> header.
+Printing can be done using any Prolog printing predicate, but the
+format-family is the most useful. See format/2.   */
+
+say_hi(_Request) :-
+        format('Content-type: text/plain~n~n'),
+        format('Hello There! Farras was here.~n').
+
+say_taco(_Request) :-
+        format('Content-type: text/plain~n~n'),
+        format('Tacos are yummie!~n').
+
+say_kebab(_Request) :-
+        format('Content-type: text/plain~n~n'),
+        format('Kebabs are quote and quote healthy!~n').
+
+handle_request(Request) :-
+%format(user_output,"I'm here~n",[]),
+%http_read_json(Request, DictIn,[json_object(term)]),
+%format(user_output,"Request is: ~p~n",[Request]),
+%format(user_output,"DictIn is: ~p~n",[DictIn]),
+%DictOut=DictIn,
+%reply_json(DictOut).
+http_read_json_dict(Request, Query),
+solve(Query, Solution),
+reply_json_dict(Solution).
+
+solve(_{a:X, b:Y}, _{answer:N}) :-
+    number(X),
+    number(Y),
+    N is X + Y.
+
+is_movie(X,Y) :- movie(X,Y).
+
