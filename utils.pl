eb56fdc0d755badc642d9e4dfba34a195604b885
butterchoco <ahmadsupri1999@gmail.com>
2021-05-22 22:25:14 +0700

    Restructure Movie Filter By Name

--- utils.pl
+++ utils.pl
@@ -1,8 +1,10 @@
 getMovieName(NameAtom, MovieName) :-
     listMovies(Movies),
     getMovieName(Movies, NameAtom, MovieName).
-
 getMovieName([X|_], Subname, X) :-
     sub_atom(X, _, _, _, Subname).
 getMovieName([_|Xt], Subname, X) :-
     getMovieName(Xt, Subname, X).
+
+getActors(Movie, List) :-
+    findall(Actor, actor(Movie, Actor, _);actress(Movie, Actor, _), List).
--- web-service.pl
+++ web-service.pl
@@ -8,6 +8,7 @@
 :- use_module(library(http/http_parameters)).
 :- [database].
 :- [utils].
+:- [controller/movieFilterByName].
 
 http:location(files, '/f', []).
 http:location(food, '/food', []).
@@ -20,19 +21,16 @@ http:location(food, '/food', []).
 
 % And, just for clarity, define a second handler
 % this one can by reached at http://127.0.0.1:8000/taco
-:- http_handler(food('tacos/'), say_taco, []).
-:- http_handler(food('kebabs/'), say_kebab, []).
-:- http_handler('/add/', handle_request, []).
 :- http_handler('/movies', get_Data, []).
 :- http_handler('/movies/', get_Data, []).
 :- http_handler('/movies/add', add_movie, []).
 :- http_handler('/movies/add/', add_movie, []).
 :- http_handler('/movies/delete', delete_movie, []).
 :- http_handler('/movies/delete/', delete_movie, []).
-:- http_handler('/test-json/', handle_json_request, []).
 :- http_handler('/movies/filter-by-year', get_movie_by_year, []).
 :- http_handler('/movies/filter-by-year/', get_movie_by_year, []).
 :- http_handler('/movies/detail', get_movie_by_name, []).
+:- http_handler('/movies/detail/', get_movie_by_name, []).
 :- http_handler('/movies/genres/', get_genres, []).
 :- http_handler('/movies/by-genre', get_movies_by_genre, []).
 :- http_handler('/movies/by-genre/', get_movies_by_genre, []).
@@ -60,28 +58,7 @@ Handlers
 
 say_hi(_Request) :-
         format('Content-type: text/plain~n~n'),
-        format('Hello There! Farras was here.~n').
-
-say_taco(_Request) :-
-        format('Content-type: text/plain~n~n'),
-        format('Tacos are yummie!~n').
-
-say_kebab(_Request) :-
-        format('Content-type: text/plain~n~n'),
-        format('Kebabs are quote and quote healthy!~n').
-
-handle_request(Request) :-
-        http_read_json_dict(Request, Query),
-        solve(Query, Solution),
-        reply_json_dict(Solution).
-
-handle_json_request(Request) :-
-   format(user_output,"I'm here~n",[]),
-   http_read_json(Request, DictIn,[json_object(term)]),
-   format(user_output,"Request is: ~p~n",[Request]),
-   format(user_output,"DictIn is: ~p~n",[DictIn]),
-   DictOut=DictIn,
-   reply_json(DictOut).
+        format('Hello There! Farras, Sayid, Supri were here.~n').
 
 get_movie_by_year(Request) :- 
         http_parameters(Request, [year(Year, [])]),
@@ -95,12 +72,6 @@ get_Data(Request) :-
         format(user_output,"Request is: ~p~n",[Request]),
         listMovies(DictOut),
         reply_json_dict(_{list:DictOut}).
-
-get_movie_by_name(Request) :- 
-        http_parameters(Request, [name(Name, [])]),
-        format(user_output,"Query is: ~p~n",[Name]),
-        movieByName(Name, DictOut),
-        reply_json(DictOut).
 
 get_genres(Request) :-        
         format(user_output,"Request is: ~p~n",[Request]),
@@ -134,12 +105,6 @@ delete_movie(Request) :-
 Handlers Method
 */
 
-solve(_{a:X, b:Y}, Dict) :-
-    number(X),
-    number(Y),
-    N is X + Y,
-    Dict = _{sum: N}.
-
 listMovies(List) :-
         setof(Movie, Year^movie(Movie,Year),List). 
         % findall(Movie, movie(Movie, _), List).
@@ -150,18 +115,6 @@ listGenres(List) :-
 movieByYear(Year, List) :- 
         atom_number(Year, YearNumber),
         findall(Movie, movie(Movie, YearNumber), List).
-
-movieByName(Name, _{moviename: MovieName, year:OutputYear, actors: ActorsList, genres: ListGenre}) :-
-        json:to_atom(Name, NameAtom),
-        getMovieName(NameAtom, MovieName),
-        format(user_output,"MovieName is: ~p~n",[MovieName]),
-        getActors(MovieName, List),
-        bagof(Year, movie(MovieName, Year), [OutputYear| _]),
-        bagof(Genre, genre(MovieName, Genre), ListGenre),
-        ActorsList = List.
-
-getActors(Movie, List) :-
-        findall(Actor, actor(Movie, Actor, _);actress(Movie, Actor, _), List).
 
 movies_by_genre(Genre, _{list:List}) :-
         json:to_atom(Genre, GenreAtom),
