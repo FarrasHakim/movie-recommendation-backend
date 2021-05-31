81b504490204b1849d32d3a8a5ec328842dfba55
butterchoco <butterchoco@pop-os.localdomain>
2021-05-31 08:53:15 +0700

    Fix filter by name

--- dao/movies/filter/name.pl
+++ dao/movies/filter/name.pl
@@ -1,11 +1,13 @@
 :- [utils].
 
 % Get movie by name
-movie_detail(Name, _{moviename: MovieName, year:OutputYear, actors: ActorsList, genres: ListGenre}) :-
+movie_detail(Name, _{movie: MovieName, year:OutputYear, actors: ActorsList, genres: ListGenre, poster: Poster, desc: Description}) :-
     json:to_atom(Name, NameAtom),
     getMovieName(NameAtom, MovieName),
     format(user_output,"MovieName is: ~p~n",[MovieName]),
     list_actors(MovieName, List),
     bagof(Year, movie(MovieName, Year), [OutputYear| _]),
     bagof(Genre, genre(MovieName, Genre), ListGenre),
+    bagof(Poster, poster(MovieName, Poster), [Poster|_]),
+    bagof(Description, description(MovieName, Description), [Description|_]),
     ActorsList = List.
