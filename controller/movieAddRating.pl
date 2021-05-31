2b40d5896c084a8e1d616b2a23b8bb4eaa29afbc
Sayid Abyan Rizal Shiddiq <sayidabyan@gmail.com>
2021-05-31 23:56:54 +0700

    Fix Rating data type from atom to number

--- controller/movieAddRating.pl
+++ controller/movieAddRating.pl
@@ -5,6 +5,7 @@ request_handler_add_rating(Request) :-
     http_parameters(Request, [user(User, []), movie(Movie, []), rating(Rating, [])]),
     json:to_atom(User, UserAtom),
     json:to_atom(Movie, MovieAtom),
-    add_rating(UserAtom, MovieAtom, Rating),
+    atom_number(Rating, RatingAtom),
+    add_rating(UserAtom, MovieAtom, RatingAtom),
     reply_json_dict(_{message:"Success"}).
     
